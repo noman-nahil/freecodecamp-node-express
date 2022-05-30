e9c472a34619ee5a9e72ab8f3d780309b8ca949a
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2022-05-30 16:58:59 +0600

    Serve static assets

--- myApp.js
+++ myApp.js
@@ -4,7 +4,8 @@ let app = express();
 console.log("Hello world")
 
 
-
+app.use(express.static(__dirname + "/public/style.css"));
+//app.use(__dirname + '/public/style.css');
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
